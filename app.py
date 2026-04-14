e381abd2b80914b152c8662d94e5302b81a637ce
saniyyyyah <saniyyahmutiara851@gmail.com>
2026-04-14 16:51:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,43 @@
 import streamlit as st
 from PIL import Image
+import numpy as np
 
 st.title("💄 Shade Lip Cream Recommendation AI")
 
 uploaded_file = st.file_uploader("Upload foto wajah kamu", type=["jpg","png","jpeg"])
 
 shades = [
-    "natural nude",
+    "deep red",
+    "brown nude",
     "warm peach",
     "soft pink",
-    "deep red",
-    "brown nude"
+    "natural nude"
 ]
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Foto kamu", use_column_width=True)
 
+    # ubah ke array
+    img = np.array(image)
+
+    # ambil rata-rata warna (brightness)
+    brightness = img.mean()
+
+    # logika sederhana
+    if brightness < 80:
+        result = shades[0]
+    elif brightness < 120:
+        result = shades[1]
+    elif brightness < 160:
+        result = shades[2]
+    elif brightness < 200:
+        result = shades[3]
+    else:
+        result = shades[4]
+
     st.success("Rekomendasi shade kamu:")
-    st.write(shades[0])
+    st.write(result)
 
 
 
