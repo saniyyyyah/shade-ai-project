b5150f82ec5e72c233bdb9fa032205475197fb39
saniyyyyah <saniyyahmutiara851@gmail.com>
2026-04-14 17:12:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,21 +10,27 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Foto kamu", use_column_width=True)
 
+    # ubah ke array
     img = np.array(image)
 
-    # ambil rata-rata warna
-   # selisih warna
-rg = r - g
-rb = r - b
-gb = g - b
+    # ambil rata-rata warna RGB
+    avg_color = img.mean(axis=(0,1))
+    r, g, b = avg_color
 
-# tentukan undertone
-if rg > 10 and rb > 10:
-    undertone = "Warm"
-elif rb < -10:
-    undertone = "Cool"
-else:
-    undertone = "Neutral"
+    st.write(f"RGB: R={int(r)}, G={int(g)}, B={int(b)}")
+
+    # hitung selisih warna
+    rg = r - g
+    rb = r - b
+
+    # tentukan undertone
+    if rg > 10 and rb > 10:
+        undertone = "Warm"
+    elif rb < -10:
+        undertone = "Cool"
+    else:
+        undertone = "Neutral"
+
     st.subheader(f"Undertone kamu: {undertone}")
 
     # rekomendasi shade
