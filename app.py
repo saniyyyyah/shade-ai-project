a95604805c5bad303dc5df5da6220f094f640652
saniyyyyah <saniyyahmutiara851@gmail.com>
2026-04-17 18:57:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,36 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 import cv2
-import torch
-import open_clip
-import torchvision.transforms as transforms
-import timm
-import torch.nn.functional as F
 
-st.set_page_config(page_title="Shade AI Hybrid", layout="centered")
-st.title("💄 Shade AI (FINAL AI UPGRADE)")
+st.title("💄 Shade AI TEST")
 
-uploaded_file = st.file_uploader("Upload foto wajah", type=["jpg","png","jpeg"])
+uploaded_file = st.file_uploader("Upload foto", type=["jpg","png","jpeg"])
 
-# =====================
-# LOAD CLIP
-# =====================
-@st.cache_resource
-def load_clip():
-    model, _, preprocess
+if uploaded_file is not None:
+    image = Image.open(uploaded_file).convert("RGB")
+    st.image(image)
+
+    img = np.array(image)
+    h, w, _ = img.shape
+
+    face = img[int(h*0.2):int(h*0.8), int(w*0.2):int(w*0.8)]
+
+    if face.size == 0:
+        st.error("Crop gagal")
+    else:
+        hsv = cv2.cvtColor(face, cv2.COLOR_RGB2HSV)
+        hue = hsv[:,:,0].mean()
+
+        if hue < 25:
+            undertone = "Warm"
+        elif hue > 95:
+            undertone = "Cool"
+        else:
+            undertone = "Neutral"
+
+        st.success(f"Undertone: {undertone}")
+
+        # hasil dummy dulu
+        st.write("💄 Coral")
+        st.write("💄 Peach")
+        st.write("💄 Nude")
